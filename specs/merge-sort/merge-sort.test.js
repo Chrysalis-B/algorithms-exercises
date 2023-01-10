52ba1aba5892b77dbbe74031a1791ecd2b294872
Heini Ahokas <heini.ahokas@klab-berlin.com>
2023-01-10 19:13:20 +0100

    feat: solve merge sort

--- specs/merge-sort/merge-sort.test.js
+++ specs/merge-sort/merge-sort.test.js
@@ -6,13 +6,31 @@
   You'll need to write more than just one function
 */
 
+const merge = (sortedArray1, sortedArray2) => {
+  const result = [];
+  while (sortedArray1.length && sortedArray2.length) {
+    if (sortedArray1[0] < sortedArray2[0]) {
+      result.push(sortedArray1.shift());
+    } else {
+      result.push(sortedArray2.shift());
+    }
+  }
+  return result.concat(sortedArray1, sortedArray2);
+};
+
 const mergeSort = (nums) => {
-  // code goes here
+  if (nums.length === 1) return nums;
+  else {
+    const half = Math.ceil(nums.length / 2);
+    const sortedFirstHalf = mergeSort(nums.slice(0, half));
+    const sortedSecondHalf = mergeSort(nums.slice(half));
+    return merge(sortedFirstHalf, sortedSecondHalf);
+  }
 };
 
 // unit tests
 // do not modify the below code
-test.skip("merge sort", function () {
+test("merge sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const ans = mergeSort(nums);
   expect(ans).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
